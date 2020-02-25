a70212bde0666842cb1519710850ffe5664cf9d7
“Shivani” <shivani_p@tekditechjoomla.com>
2020-02-25 11:35:57 +0530

    Bug #158103 fix: User List - Sort by User name, T&C Title, T&C Code, Version and T&C Accepted date are not working

--- src/components/com_tc/administrator/models/usertcs.php
+++ src/components/com_tc/administrator/models/usertcs.php
@@ -35,6 +35,7 @@ class TcModelUsertcs extends JModelList
 				'title', 'c.`title`',
 				'client', 'c.`client`',
 				'accepted_date', 'a.`accepted_date`',
+				'version', 'a.`version`'
 			);
 		}
 
