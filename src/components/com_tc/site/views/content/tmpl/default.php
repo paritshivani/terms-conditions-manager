88fdd8d880916b0ba45c5150acc093fabbd2a73f
“Shivani” <shivani_p@tekditechjoomla.com>
2020-03-06 14:52:33 +0530

    Feedback #158201 fix: frontend layout change

--- src/components/com_tc/site/views/content/tmpl/default.php
+++ src/components/com_tc/site/views/content/tmpl/default.php
@@ -14,14 +14,14 @@ $document = JFactory::getDocument();
 
 // Add styles
 $style = '.tc-box {'
-        . 'border-top-style: ridge;'
-        . 'border-right-style: hidden;'
-        . 'border-bottom-style: ridge;'
-        . 'border-left-style: hidden;'
-        . 'overflow-y: auto;'
-		. 'max-height: 300px;'
-		. 'overflow-x: hidden;'
-        . '}';
+					. 'border-top-style: ridge;'
+					. 'border-right-style: hidden;'
+					. 'border-bottom-style: ridge;'
+					. 'border-left-style: hidden;'
+					. 'overflow-y: auto;'
+					. 'max-height: 300px;'
+					. 'overflow-x: hidden;'
+					. '}';
 $document->addStyleDeclaration($style);
 ?>
 		<script>
@@ -41,45 +41,50 @@ $document->addStyleDeclaration($style);
 				}
 			}
 		</script>
-		<?php if (!empty($this->tc_id))
+		<?php
+		if (!empty($this->tc_id))
 		{
-			?>
-			<div class="well well-condensed">
+		?>
+		<div class="well well-condensed">
 			<div class="">
 				<h1 class="center"><?php echo $this->termsandconditions->title; ?></h1>
-				<div class="center"><strong><?php echo JText::_("COM_TC_LATEST_TERMSANDCONDITIONS_VERSION") . $this->termsandconditions->version; ?>&nbsp;&nbsp;
-				</strong></div>
+				<div class="center">
+					<strong>
+						<?php echo JText::_("COM_TC_LATEST_TERMSANDCONDITIONS_VERSION") . $this->termsandconditions->version; ?>&nbsp;&nbsp;
+					</strong>
+				</div>
 			</div>
 			<br>
 			<div class="tc-box">
 				<?php echo nl2br($this->termsandconditions->content);
 		}?>
 			</div>
-<?php
-		$app        = JFactory::getApplication();
-		$input      = $app->input;
-		$returnURL     = $input->get('return', '', 'STRING');
-?>
+			<?php
+			$app        = JFactory::getApplication();
+			$input      = $app->input;
+			$returnURL     = $input->get('return', '', 'STRING');
+			?>
 
-		<div class="">
-			 <form action="" method="post" name="form" onsubmit="return validateform(this)">
-			 <div class="checkbox">
-				<label class="padded-l-0"><input id="agree" type="checkbox" name="accept" value="1">
-				<?php	echo JText::_('COM_TC_LATEST_TERMSANDCONDITIONS_AGREE'); ?></label>
-				<div class="pull-right">
-			<button class="btn btn-primary" type="submit" value="Submit" name="Submit">
-			<?php echo JText::_('COM_TC_ACCEPT_TERMSANDCONDITIONS_BUTTON'); ?></button>
+			<div class="">
+					<form action="" method="post" name="form" onsubmit="return validateform(this)">
+					 	<div class="checkbox">
+							<label class="padded-l-0">
+								<input id="agree" type="checkbox" name="accept" value="1">
+								<?php echo JText::_('COM_TC_LATEST_TERMSANDCONDITIONS_AGREE');?>
+							</label>
+							<div class="pull-right">
+								<button class="btn btn-primary" type="submit" value="Submit" name="Submit">
+								<?php echo JText::_('COM_TC_ACCEPT_TERMSANDCONDITIONS_BUTTON'); ?></button>
+							</div>
+						</div>
+						<input type="hidden" name="option" value="com_tc">
+						<input type="hidden" name="task" value="content.accept()">
+						<input type="hidden" name="user_id" value="<?php echo $this->user_id; ?>">
+						<input type="hidden" name="tc_id" value="<?php echo $this->tc_id; ?>">
+						<input type="hidden" name="return_url" value="<?php echo $returnURL; ?>">
+						<strong><?php echo JText::_("COM_TC_LATEST_TERMSANDCONDITIONS_UPDATED_DATE") .
+							HTMLHelper::_('date', $this->termsandconditions->modified_on, $this->dateFormat, 'UTC');?>
+						</strong>
+					</form>
 			</div>
-			</div>
-			<input type="hidden" name="option" value="com_tc">
-			<input type="hidden" name="task" value="content.accept()">
-			<input type="hidden" name="user_id" value="<?php echo $this->user_id; ?>">
-			<input type="hidden" name="tc_id" value="<?php echo $this->tc_id; ?>">
-			<input type="hidden" name="return_url" value="<?php echo $returnURL; ?>">
-			<strong><?php echo JText::_("COM_TC_LATEST_TERMSANDCONDITIONS_UPDATED_DATE") .
-				HTMLHelper::_('date', $this->termsandconditions->modified_on, $this->dateFormat, 'UTC');?>
-			</strong>
-
-			</form>
-		</div>
 		</div>
